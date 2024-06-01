fedcb684ea36be152e01812f87204409472c594b
NJCN <89707155+itsnjcn@users.noreply.github.com>
2024-06-01 22:32:21 +0400

    minor improvements

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env.SIGNALING_SERVER_URL': JSON.stringify(process.env.SIGNALING_SERVER_URL),
-        })
-    ],
-    }
